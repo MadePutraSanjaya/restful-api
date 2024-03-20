d62f447813420694ed710233ace50420447acc6c
I Made Adi Putra Sanjaya <madeputra.6166@gmail.com>
2024-03-20 13:56:31 +0800

    todo: update migrate

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,20 +1,18 @@
-// File: migrate.go
-
 package main
 
 import (
-    "crud/initializers"
-    "crud/models"
+	"crud/initializers"
+	"crud/models"
 )
 
 func main() {
-    db, err := initializers.ConnectToDb()
-    if err != nil {
-        panic(err)
-    }
+	db, err := initializers.ConnectToDb()
+	if err != nil {
+		panic(err)
+	}
 
-    err = db.AutoMigrate(&models.Post{})
-    if err != nil {
-        panic(err)
-    }
+	err = db.AutoMigrate(&models.Post{})
+	if err != nil {
+		panic(err)
+	}
 }
